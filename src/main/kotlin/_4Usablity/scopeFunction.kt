aa4f05dc79678d76b86dce5969fbc4175aedb0ac
Ankitgujare <ankitgujare001@gmail.com>
2026-04-25 00:17:12 +0530

    Scoped functions In Kotlin

--- src/main/kotlin/_4Usablity/scopeFunction.kt
+++ src/main/kotlin/_4Usablity/scopeFunction.kt
@@ -1,8 +1,12 @@
 package _4Usablity
 
 fun main(){
-    letdemo(null)
-    rundemo("Ankit")
+   /* letdemo(null)
+    rundemo("Ankit")*/
+    withdemo()
+    applydemo()
+    aslodemo()
+
 }
 
 
@@ -25,3 +29,51 @@ fun rundemo(name:String?) {
     }
     println(x)
 }
+
+
+//with
+//chain multiple Operations on an Object
+
+fun withdemo(){
+    val user=User()
+
+   with(user){
+        name="Ankit"
+        age=12
+        gender="Male"
+    }
+
+    println(user.age)
+    println(user.name)
+    println(user.gender)
+}
+
+
+//used to config an Object
+fun applydemo(){
+    val user=User().apply {
+        name="Alex"
+        age=22
+        gender="Male"
+    }
+
+    println("${user.age} ${user.gender} ${user.name}")
+}
+
+
+class User{
+    var name=""
+    var age=1
+    var gender=""
+}
+
+
+fun aslodemo(){
+    val list=mutableListOf(1,2,3).also {
+        println("Before: $it")
+        it.add(4)
+        println("After: $it")
+    }
+}
+
+

8f396cc718b9c3256b6bb9432b40a55299b15222
Ankitgujare <ankitgujare001@gmail.com>
2026-05-09 16:10:09 +0530

    Class Delegation In kt

---
+++ src/main/kotlin/ObjectOrientedPrograming05/ClassDelegation.kt
@@ -0,0 +1,85 @@
+package ObjectOrientedPrograming05
+
+
+
+
+fun main(){
+    val freelancer=freelancer()
+    freelancer.code()
+
+    val intern=intern()
+    intern.code()
+
+    val freelancer1=freelancer1()
+    freelancer1.code()
+
+    val fullstackDev=fullstackDev()
+    fullstackDev.code()
+    fullstackDev.codebackend()
+
+}
+
+
+interface programmer{
+    fun code()
+}
+
+class KotlinProg(val laptop:String):programmer{
+    override fun code() {
+        println("Programing in Kotlin in $laptop")
+    }
+
+}
+
+
+
+
+
+class freelancer:programmer by KotlinProg("Hp")
+
+//we can aslo Override an implementation of Delegated class Like
+
+class intern(
+    private val kotlinProg: KotlinProg = KotlinProg("Mac")
+) : programmer by kotlinProg {
+
+    override fun code() {
+        println("Intern coding in Kotlin using ${kotlinProg.laptop}")
+    }
+}
+//we can aslo pass the parameters Like
+class freelancer1:programmer by KotlinProg("Desktop")
+
+
+//You cannot delegate to two Diffrent classes that uses same Interface
+
+class JavaProgramer:programmer{
+    override fun code() {
+        println("Coding in Java")
+    }
+
+}
+/*
+class freelancer4:programmer by KotlinProg("mac"),
+        programmer by JavaProgramer()*/
+
+
+
+//Instead we can do
+//create an Another interface
+
+interface backEndDeveloper{
+    fun codebackend()
+}
+
+class intern2:backEndDeveloper{
+    override fun codebackend() {
+        println("Coding in Java developing backend")
+    }
+}
+
+
+class fullstackDev:programmer by JavaProgramer(),
+        backEndDeveloper by intern2()
+
+

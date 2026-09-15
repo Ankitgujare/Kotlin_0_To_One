3e078e7326dcb56e324fc65cd0e9376f1d1a177c
Ankitgujare <ankitgujare001@gmail.com>
2026-09-15 21:57:04 +0530

    Scanner Class

--- src/main/kotlin/_4Usablity/InputEx.java
+++ src/main/kotlin/_4Usablity/InputEx.java
@@ -25,17 +25,60 @@ public class InputEx {
         System.out.println(sum);*/
 
 
-        InputStreamReader reader=new InputStreamReader(System.in);
-        BufferedReader bufferedReader=new BufferedReader(reader);
 
-        System.out.println("Input something");
-        int x=Integer.parseInt(bufferedReader.readLine());
-        System.out.println(x);
+
+   /*     InputStreamReader reader=new InputStreamReader(System.in);
+        BufferedReader bufferedReader=new BufferedReader(reader);
+        int a= Integer.parseInt(bufferedReader.readLine());
+        float b=Float.parseFloat(bufferedReader.readLine());
+        String name=bufferedReader.readLine();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        //String,float,Int, double,Long,byte*/
+
+
 
         /**
          *
          *Scanner class ***
+         * USED to take Input from user At runtime
+         *
+         * 1.create an Obj of Scanner class
          */
+        Scanner sc=new Scanner(System.in);
+        //int
+      /*  int a=sc.nextInt();
+        float b=sc.nextFloat();
+        long c=sc.nextLong();
+        double d=sc.nextDouble();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(d);
+        System.out.println(c);*/
+        /*char name=sc.next().charAt(0);*/
+
+        System.out.println("Enter the name");
+        String name=sc.nextLine();
+        System.out.println("Hi "+name+" Are You smart Press 1 if yes Otherwise 2 to say No");
+        int choice=sc.nextInt();
+        if (choice!=1){
+            //Not smart
+            boolean isSmart=false;
+            System.out.println(isSmart);
+        }else{
+            boolean isSmart=true;
+            System.out.println(isSmart);
+        }
+
+
+
+
+
+
+
+
+
 
 
     }
